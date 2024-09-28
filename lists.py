08af91b3918885f34892c818d481fd1612637ea6
abhinav37-git <abhinavd-11@outlook.com>
2024-09-28 21:41:11 +0530

    First commit

---
+++ lists.py
@@ -0,0 +1,24 @@
+#creating a list
+mylist = ["abhinav", "abhishek"]
+print(mylist)
+
+#accessing the element of list at particular element
+item = mylist[1]
+print(item)
+
+#multiple datatypes inside the list
+# mylist2 = ["abhinav", 5, True, 0]
+# print(mylist2)
+# item1 = mylist2[-1]
+# print(item1)
+
+#iterating inside the list
+# for i in mylist2:
+#     print(i)
+
+#if else condition inside the list
+
+if "abhinav" in mylist:
+    True
+else:
+    False
